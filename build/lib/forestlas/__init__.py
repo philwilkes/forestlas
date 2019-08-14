d38507093347f71365b2b709c2a3656655c63c18
philwilkes <p.wilkes@ucl.ac.uk>
2019-08-14 12:58:35 +0100

    removed build dir

--- build/lib/forestlas/__init__.py
+++
@@ -1,9 +0,0 @@
-# http://www.diveintopython3.net/case-study-porting-chardet-to-python-3.html#multifile-modules
-
-def detect(aBuf):
-    from . import universaldetector
-    u = universaldetector.UniversalDetector()
-    u.reset()
-    u.feed(aBuf)
-    u.close()
-    return u.result
